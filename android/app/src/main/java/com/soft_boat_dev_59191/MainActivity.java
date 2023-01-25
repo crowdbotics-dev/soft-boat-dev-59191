5b874fa8d77251d18ed94f9119979f392f92cbae
Crowdbotics <engineering@crowdbotics.com>
2023-01-25 00:07:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/soft_boat_dev_59191/MainActivity.java
@@ -0,0 +1,15 @@
+package com.soft_boat_dev_59191;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "soft_boat_dev_59191";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-boat-dev-59191",
+  url: "https://soft-boat-dev-59191.botics.co",
+  api: "https://soft-boat-dev-59191.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
